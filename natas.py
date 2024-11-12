99e1b28817c02b8699ea04f94480649505d6e4ec
bodhisatva <hmh.uhtala11@gmail.com>
2024-11-12 10:11:13 +0200

    (natas): prompt inside create url suffix

--- natas.py
+++ natas.py
@@ -13,6 +13,7 @@ from pygments.formatters import TerminalFormatter
 
 YELLOW='\033[33m'
 GREEN='\33[92m'
+RED='\033[31m'
 
 def get_response_data(username: str, password: str, url_suffix: str):
   url = f'http://{username}.natas.labs.overthewire.org/{url_suffix}'
@@ -22,10 +23,10 @@ def get_response_data(username: str, password: str, url_suffix: str):
 
   if status_code != 200:
     if status_code == 401:
-      print(f'\nAuthentication failed.\nStatus code of {status_code} returned.')
+      print(f'\n{RED}Authentication failed.{GREEN}\nStatus code of {YELLOW}{status_code}{GREEN} returned.')
       exit(1)
     else:
-      print(f'\nSomething went wrong. Status code of {status_code} returned.')
+      print(f'\n{RED}Something went wrong.{GREEN} Status code of {YELLOW}{status_code}{GREEN} returned.')
       exit(1)
   
   return response.content.decode()
@@ -36,24 +37,24 @@ def print_results(prettified_html, password_sentence):
   if password_sentence:
     print(f"{password_sentence[0]}\n")
 
-def create_url_suffix(suffix: str):
-  if suffix == option1:
-    return click.prompt("Enter URL suffix")
+
+def create_url_suffix():
+  choice = click.prompt(f"Enter {YELLOW}1{GREEN} to add URL suffix or press {YELLOW}ENTER{GREEN}", 
+default="")
+  if choice == "1":
+    suffix = click.prompt("Enter URL suffix")
+    return suffix
   else:
     return ""
-
-option1 = "1"
-option2 = "2"
 
 print(f"\n{YELLOW}STARTING RECOINNAISSANCE{GREEN}\n")
 
 @click.command()
 @click.option('--username', '-u', prompt="Enter username")
 @click.option('--password', '-p', prompt="Enter password")
-@click.option('--url_suffix', '-url', type=click.Choice([option1, option2]), prompt="1: Add URL suffix\n2: Leave empty")
 
-def main(username: str, password: str, url_suffix: str):
-  suffix = create_url_suffix(url_suffix)
+def main(username: str, password: str):
+  suffix = create_url_suffix()
   html = get_response_data(username, password, suffix)
   html_content = BeautifulSoup(html, 'html.parser')
 
